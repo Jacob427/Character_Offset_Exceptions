bc3e030db0eb379be3413a7fc334e0b3f2a1fb79
an <n@n.com>
2022-07-22 12:00:53 -0400

    Added try/catch under the function to check if entry is valid letter

--- Source.cpp
+++ Source.cpp
@@ -7,20 +7,26 @@ CIS 1202 800
 #include <iostream>
 using namespace std;
 
-char character(char start, int offset);
-
-
-int main() {
-	char letter = 'A';
-	int offset = 23;
-
-	cout << character(letter, offset);
-
-
-}
-
-
 char character(char start, int offset) {
+	if (!isalpha(start)) {	//test for valid letter/start a-z or A-Z
+		string invalidCharacterExcpeption = "ERROR: Must be a valid letter!";
+		throw invalidCharacterExcpeption;
+	}
 	char result = start + offset;
 	return result;
+}
+
+int main() {
+	char letter = 'z';
+	int offset = 23;
+	char new_letter;
+
+	try {
+		new_letter = character(letter, offset);
+		cout << new_letter << endl;
+	}
+	catch (string invalidCharacterExcpeption) {
+		cout << invalidCharacterExcpeption << endl;
+	}
+
 }
